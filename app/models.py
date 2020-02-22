b6441c07e017bc72b7b02a9c6b93426705396a67
dnjoe96 <donjoedbest@gmail.com>
2020-02-22 12:53:53 +0100

    implimented admin and personnel login

--- app/models.py
+++ app/models.py
@@ -1,3 +1,42 @@
 from app import db
+from datetime import datetime
 
 
+class Personnel(db.Model):
+    __tablename__ = 'personnel'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    update_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+    def __repr__(self):
+        return '<username {}>'.format(self.username)
+
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname = db.Column(db.String(30), nullable=False)
+    lastname = db.Column(db.String(30), nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    register_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, username, lastname, firstname, pasword_hash):
+        self.username = username
+        self.lastname = lastname
+        self.firstname = firstname
+        self.password_hash = pasword_hash
+
+    def __repr__(self):
+        return '<username {}>'.format(self.username)
+
+
+class Car_Registration(db.Model):
+    pass
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
-from flask import render_template, request, redirect, url_for
-from app import app
+from flask import render_template, request, redirect, url_for, flash, session
+from app import app, db
+from functools import wraps
+from app.models import Personnel, Admin, Car_Registration
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized access, Please login', 'danger')
+            return redirect(url_for('login'))
+
+    return wrapped
 
 
 @app.routes('/', methods=['GET', 'POST'])
@@ -8,7 +23,27 @@ def login():
     login page will be the route for login, the get request will get the form and render
     html, the post will handle submission
     '''
-    pass
+
+    if request.method == 'POST':
+        # get form field
+        username = request.form['username']
+        password = request.form['password']
+
+        if username and password:
+            find = Personnel.Query.filter_by(username=username).one()
+            verify = check_password_hash(find.password_hash, password)
+
+            if verify:
+                flash('successfully logged in', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Invalid username or password, try again or visit admin to reset password', 'danger')
+                return redirect(url_for('login'))
+        else:
+            flash('Invalid input')
+            return redirect(url_for('login'))
+
+    return render_template('index.html')
 
 
 @app.routes('/home', methods=['GET', 'POST'])
@@ -19,6 +54,34 @@ def home():
     validate from database.
     '''
     pass
+
+
+@app.route('/admin/sign_up')
+def admin_sign_up():
+    ''' Admin sign up, where admins are registered '''
+
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        username = request.form['username']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+
+        find = Personnel.Query.filter_by(username=username)
+
+        if not find:
+            save = Admin(username, lastname, firstname, password_hash)
+            db.session.add(save)
+            db.session.commit()
+            flash("successfully registered", "success")
+            return redirect(url_for('main'))
+
+        else:
+            flash('user already exists')
+            return redirect(url_for('presonnel_register'))
+
+    return render_template('personnel_register.html')
+
 
 
 @app.routes('/admin')
@@ -33,7 +96,7 @@ def admin():
 @app.routes('/admin/register', methods=['GET', 'POST'])
 def vehicle_register():
     '''
-    This is the route for registeratio of users, it should be on the Nav bar on a side br in the admin panel.
+    This is the route for registration of users, it should be on the Nav bar on a side br in the admin panel.
     on get, it renders the data entry form, on post, it submits to the database
     :return:
     '''
@@ -56,6 +119,25 @@ def personnel_register():
     on get, it renders the data entry form, on post, it submits to the database
     :return:
     '''
-    pass
+
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+
+        find = Personnel.Query.filter_by(username=username)
+
+        if not find:
+            save = Personnel(username, password_hash)  # sender ID will be passed in here when i obtain it
+            db.session.add(save)
+            db.session.commit()
+            flash("successfully registered", "success")
+            return redirect(url_for('main'))
+
+        else:
+            flash('user already exists')
+            return redirect(url_for('presonnel_register'))
+
+    return render_template('personnel_register.html')
 
 
